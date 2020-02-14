281361f2b648aedba9afef6710b1db46603d210f
Cesar Fernandez <32900036+cesfernandez1@users.noreply.github.com>
2020-02-14 14:20:16 +0100

    Added Options Screen

---
+++ Hundir_la_Flota/Assets/Scripts/LoadScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadScreen : MonoBehaviour
+{
+
+    public Slider mySlider;
+    public GameObject loadScreen;
+    public GameObject mainMenuScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        while (mySlider.value != 1)
+        {
+            mySlider.value += 0.000001f;
+            UnityEngine.Debug.Log(mySlider.value);
+        }
+
+        if(mySlider.value == 1)
+        {
+            loadScreen.SetActive(false);
+            mainMenuScreen.SetActive(true);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Hundir_la_Flota/Assets/Scripts/MainMenu.cs
+++ Hundir_la_Flota/Assets/Scripts/MainMenu.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,12 +28,14 @@ public class MainMenu : MonoBehaviour
 
     public void OpenOptions()
     {
-
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
     }
 
-    public void ClosOptions()
+    public void CloseOptions()
     {
-
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
     }
 
     public void CloseGame()
